f427adc6f4636fb875c18ca36307620bc4d47355
Conor Howard <47044266+conorhoward6510@users.noreply.github.com>
2025-05-31 19:09:59 +0100

    Initial Commit

---
+++ Jokes App Challenge/Jokes_App_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_App_ChallengeApp.swift
+//  Jokes App Challenge
+//
+//  Created by Conor Howard on 31/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_App_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
